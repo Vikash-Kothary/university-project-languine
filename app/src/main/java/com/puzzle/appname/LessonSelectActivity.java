3eeecc3943ff685d0e3ed0aae46db3311ed7a6a9
Wahab Hassan <Wahab_211715@Hotmail.co.uk>
2016-02-18 20:12:16 +0000

    added the video

--- app/src/main/java/com/puzzle/appname/LessonSelectActivity.java
+++ app/src/main/java/com/puzzle/appname/LessonSelectActivity.java
@@ -2,20 +2,16 @@ package com.puzzle.appname;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Button;
 
 public class LessonSelectActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -124,6 +120,9 @@ public class LessonSelectActivity extends AppCompatActivity
             case R.id.nav_text_questions:
                 i = new Intent(this, ExerciseActivity.class);
                 //i.putExtra();
+                break;
+            case R.id.nav_Video:
+                i = new Intent(this, VideoActivity.class);
                 break;
         }
         if(i!=null){
