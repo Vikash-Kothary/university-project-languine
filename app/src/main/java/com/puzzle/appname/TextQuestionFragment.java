3104e8e1e9a1aec75f940020a7ca36c906349508
Siddhartha Singhal <sidd_singhal@live.co.uk>
2016-02-18 18:53:07 +0000

    Cake
    Copyed Code to fragment and changed from RadioButtons to Buttons

--- app/src/main/java/com/puzzle/appname/TextQuestionFragment.java
+++ app/src/main/java/com/puzzle/appname/TextQuestionFragment.java
@@ -1,10 +1,13 @@
 package com.puzzle.appname;
 
+import android.app.Activity;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -12,11 +15,37 @@ import android.view.ViewGroup;
 public class TextQuestionFragment extends Fragment {
 
     public TextQuestionFragment() {
+
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_text_question, container, false);
+        View view = inflater.inflate(R.layout.fragment_text_question, container, false);
+        TextView titleText = (TextView) view.findViewById(R.id.titleText);
+        titleText.setText("Girls");
+
+
+        TextView btext = (TextView) view.findViewById(R.id.textb);
+        btext.setText(R.string.Question);
+
+        // add the questions and answers in intent so that they can be directly added to the activity
+        ViewGroup Group = (ViewGroup) view.findViewById(R.id.radioGroup);
+        Button Button;
+        Button = new Button(this.getContext());
+        Button.setText("Answer 1");
+        Button Button2 = new Button(this.getContext());
+        Button2.setText("Answer 2");
+        Button Button3 = new Button(this.getContext());
+        Button3.setText("Answer 3");
+        Group.addView(Button);
+        Group.addView(Button2);
+        Group.addView(Button3);
+
+        return view;
+
+
+
     }
 }
