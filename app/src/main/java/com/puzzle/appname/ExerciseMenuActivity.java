86c2c0cee5fe64c7aefe9376946dbc44493750b1
tammaram <tamara.manasieva@kcl.ac.uk>
2016-02-18 18:17:36 +0000

    Buttons are showing correctly in Exercise Menu.
    Buttons created using a for loop.

--- app/src/main/java/com/puzzle/appname/ExerciseMenuActivity.java
+++ app/src/main/java/com/puzzle/appname/ExerciseMenuActivity.java
@@ -1,17 +1,13 @@
 package com.puzzle.appname;
 
-import android.app.ActionBar;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.RelativeLayout;
-
-import java.lang.reflect.Array;
+import android.widget.LinearLayout;
 
 public class ExerciseMenuActivity extends AppCompatActivity {
     String[] menu = {"Observe", "Reflect", "Experiment"};
@@ -36,22 +32,16 @@ public class ExerciseMenuActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
-    private void createButtons(){
-        RelativeLayout rl = (RelativeLayout) findViewById(R.id.content_exercise_menu);
-        RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+    private void createButtons() {
+        LinearLayout rl = (LinearLayout) findViewById(R.id.content_exercise_menu);
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT);
 
-
-        int prevButID = 0;
-        for (int i=0; i<menu.length; i++){
-            if(i == 0) { params.addRule(RelativeLayout.ALIGN_PARENT_TOP);}
-            else { params.addRule(RelativeLayout.BELOW,prevButID); }
-
+        for (int i = 0; i < menu.length; i++) {
             Button b = new Button(this);
-            b.setId(i+5);
             b.setLayoutParams(params);
             b.setText(menu[i]);
             rl.addView(b);
-            prevButID = b.getId();
         }
     }
 
