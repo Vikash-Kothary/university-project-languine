7c2938569f2d193efef33a079a3cb2b8d3c927e2
tammaram <tamara.manasieva@kcl.ac.uk>
2016-02-18 19:18:16 +0000

    Result Activity created.
    The user can review all questions and see the score.

--- app/src/main/java/com/puzzle/appname/ResultActivity.java
+++ app/src/main/java/com/puzzle/appname/ResultActivity.java
@@ -1,6 +1,5 @@
 package com.puzzle.appname;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,12 +7,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
-public class QuizActivity extends AppCompatActivity {
+public class ResultActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_quiz);
+        setContentView(R.layout.activity_result);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -25,10 +24,6 @@ public class QuizActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
-    }
-
-    private void nextButtonClicked(){
-        Intent i = new Intent(this, ResultActivity.class);
     }
 
 }
