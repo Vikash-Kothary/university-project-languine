9bae4150f6236df98c9d02bd7d11b6bad5d188b5
faucomte <f.aucomte@hotmail.co.uk>
2016-02-24 18:12:28 +0000

    Created GlossaryController class. Deleted some unnecessary code.

--- app/src/main/java/com/puzzle/appname/Sounds.java
+++ app/src/main/java/com/puzzle/appname/Sounds.java
@@ -11,20 +11,6 @@ public class Sounds {
 
     static MediaPlayer mySound;
 
-    private long id;
-    private String title;
-    private String artist;
-
-    public Sounds(long songID, String songTitle, String songArtist) {
-        id=songID;
-        title=songTitle;
-        artist=songArtist;
-    }
-
-    public long getID(){return id;}
-    public String getTitle(){return title;}
-    public String getArtist(){return artist;}
-
     Sounds(Context myContext)
     {
         Log.e("SOUND","Music file id: "+R.raw.background);
