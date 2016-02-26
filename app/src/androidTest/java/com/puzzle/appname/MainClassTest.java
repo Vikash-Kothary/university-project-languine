305453f02f874d6aa04b6d072718f84e36e3ac3a
Wahab Hassan <Wahab_211715@Hotmail.co.uk>
2016-02-26 15:21:26 +0000

    fixed some problems with testing and nav bar

--- app/src/androidTest/java/com/puzzle/appname/MainClassTest.java
+++ app/src/androidTest/java/com/puzzle/appname/MainClassTest.java
@@ -7,29 +7,38 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static android.support.test.InstrumentationRegistry.getInstrumentation;
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
 import static android.support.test.espresso.action.ViewActions.click;
-import static android.support.test.espresso.action.ViewActions.swipeRight;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 
 @RunWith(AndroidJUnit4.class)
 public class MainClassTest {
 
-    @Rule public final ActivityRule<LessonSelectActivity> main = new ActivityRule<>(LessonSelectActivity.class);
+    @Rule
+    public final ActivityRule<LessonSelectActivity> main = new ActivityRule<>(LessonSelectActivity.class);
 
     @Test
-    public void Button1(){
+    public void Button1() {
 
         onView(withId(R.id.card_list)).check(ViewAssertions.matches(isDisplayed()));
-
-        onView(withId(R.id.card_list)).perform(click(), swipeRight(), click());
-
-
-//        onData(hasToString(startsWith("Lock")))
-//                .inAdapterView(withId(R.id.card_list))
-//                .perform(click());
+        openActionBarOverflowOrOptionsMenu(getInstrumentation().getTargetContext());
+        onView(withText("Settings")).perform(click());
+        onView(withText("Lock Screen")).perform(click());
+        onView(withId(R.id.login_form)).check(ViewAssertions.matches(isDisplayed()));
+        onView(withId(R.id.email_login_form)).check(ViewAssertions.matches(isDisplayed()));
+        onView(withId(R.id.email)).check(ViewAssertions.matches(isDisplayed()));
+        onView(withId(R.id.email)).perform(typeText("cake@baker"), closeSoftKeyboard());
+        onView(withId(R.id.password)).check(ViewAssertions.matches(isDisplayed()));
+        onView(withId(R.id.password)).perform(typeText("bakes"), closeSoftKeyboard());
+        onView(withId(R.id.email_sign_in_button)).check(ViewAssertions.matches(isDisplayed()));
+        onView(withId(R.id.email_sign_in_button)).perform(click());
     }
 
 
--- app/src/main/java/com/puzzle/appname/LessonSelectActivity.java
+++ app/src/main/java/com/puzzle/appname/LessonSelectActivity.java
@@ -2,20 +2,19 @@ package com.puzzle.appname;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class LessonSelectActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,10 +28,8 @@ public class LessonSelectActivity extends AppCompatActivity
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.setDrawerListener(toggle);
         toggle.syncState();
-
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
-
         setupRecyclerView();
 
     }
@@ -81,6 +78,8 @@ public class LessonSelectActivity extends AppCompatActivity
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+            drawer.openDrawer(GravityCompat.START);
             return true;
         }
 
