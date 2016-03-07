eb5eb346e50608834dda202fd511d6c4db8e74ed
Wahab Hassan <Wahab_211715@Hotmail.co.uk>
2016-03-07 15:21:07 +0000

    trying to subtitle

--- app/src/main/java/com/puzzle/appname/VideoFragment.java
+++ app/src/main/java/com/puzzle/appname/VideoFragment.java
@@ -1,5 +1,7 @@
 package com.puzzle.appname;
 
+import android.annotation.TargetApi;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -8,22 +10,30 @@ import android.view.ViewGroup;
 import android.widget.MediaController;
 import android.widget.VideoView;
 
+import java.util.Locale;
+
+import static android.media.MediaFormat.createSubtitleFormat;
+
 public class VideoFragment extends Fragment {
-    View view;
+    private View view;
+
     public VideoFragment() {
         // Required empty public constructor
     }
 
+    @TargetApi(Build.VERSION_CODES.KITKAT)
     public void runVideo(int whichVideo){
         VideoView videoView = (VideoView)view.findViewById(R.id.videoView);
-        videoView.setVideoPath("android.resource://" +this.getContext().getPackageName() + "/" + whichVideo);
+        videoView.setVideoPath("android.resource://" + getContext().getPackageName() + "/" + whichVideo);
+System.out.println((getContext().getResources().openRawResource(R.raw.subsu01)));
+            videoView.addSubtitleSource(getContext().getResources().openRawResource(R.raw.subsu01), createSubtitleFormat("text/srt", Locale.ENGLISH.getLanguage()));
+
         videoView.setMinimumWidth(720);
         videoView.start();
 
-        MediaController controller = new MediaController(this.getContext());
+        MediaController controller = new MediaController(getContext());
         controller.setAnchorView(videoView);
         controller.setMediaPlayer(videoView);
-
         videoView.setMediaController(controller);
     }
 
