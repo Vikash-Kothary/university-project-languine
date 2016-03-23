1d11a6b6c6795322eb9d9c7247cd551ef8819a02
nammzzi <namu-ujin.natsagnyam@kcl.ac.uk>
2016-03-23 23:07:11 +0000

    mex details added

--- app/src/main/java/com/puzzle/appname/Festividades.java
+++ app/src/main/java/com/puzzle/appname/Festividades.java
@@ -10,18 +10,20 @@ import java.util.ArrayList;
 public class Festividades extends AppCompatActivity {
 
     private int[] span_images = {R.drawable.fest1,R.drawable.fest2,R.drawable.fest3, R.drawable.fest4, R.drawable.fest5, R.drawable.fest6,R.drawable.fest7, R.drawable.fest8, R.drawable.fest9, R.drawable.fest10};
-    //private int[] mex_images = {R.drawable.fest1,R.drawable.fest2};
+    private int[] mex_images = {R.drawable.fest1, R.drawable.fest2, R.drawable.fest4, R.drawable.fest_mex4, R.drawable.fest_mex5, R.drawable.fest6,R.drawable.fest_mex7, R.drawable.fest_mex8, R.drawable.fest_mex9, R.drawable.fest10};
 
     private String[] dates = {"Uno de enero","Seis de enero", "Marzo o abril", "Uno de mayo", "25 de julio", "12 de octubre", "Uno de noviembre", "Seis de diciembre", "Ocho de diciembre", "25 de diciembre"};
     private String[] spanishNames = {"Año Nuevo"," Día de Reyes", "Semanta santa", "Día del Trabajo", "Día de Santiago", "Día de la Hispanidad", "Día de Todos los Santos", "Día de la Constitucíon Española", "Inmaculada Conceptíon", "Día de la Navidad"};
     private String[] englishNames = {"New Year's Day","Epiphany", "Easter(March or April)", "Labour Day", "St James' Day", "Columbus Day(National Day)", "All Saints' Day", "Constitution Day", "Immaculate Conception", "Christmas Day"};
 
-
+    private String[] mexDates = {"Primero de enero","El 6 de enero", "Primero de mayo", "El Cinco de mayo", "16 de septiembre", "El 12 de octubre", "El 2 de noviembre", "El 20 de noviembre", "12 de diciembre", "El 25 de diciembre"};
+    private String[] mexNames = {"El año nuevo","El Día de los Reyes Magos", "Día del Trabajo", "La Batalla de Puebla", "La Independencia de México", "El Descumbrimiento de América", "El Día de los Muertos", "La Revolución de Mexcicana", "Día de la Virgen de Guadalupe", "La Navidad"};
+    private String[] mexEnglishNames = {"New Year's Day","Epiphany", "Labour Day", "5th May-Batalla of Puebla", "Independence Day", "Discovery of Americas", "Day of the Dead", "Revolution Day", "Day of the virgin of Guadalupe", "Christmas Day"};
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_festividades_spa);
+        setContentView(R.layout.activity_festividades_spa_mex);
 
         setupRecyclerView();
     }
@@ -38,7 +40,7 @@ public class Festividades extends AppCompatActivity {
         ArrayList<Festividad> myDataset = new ArrayList<Festividad>();
         for (int i = 0; i < dates.length; ++i)
         {
-            myDataset.add(new Festividad(span_images[i], dates[i], spanishNames[i], englishNames[i]));
+            myDataset.add(new Festividad(mex_images[i], mexDates[i], mexNames[i], mexEnglishNames[i]));
         }
 
         FestividadesAdapter festividadesAdapter = new FestividadesAdapter(myDataset);
