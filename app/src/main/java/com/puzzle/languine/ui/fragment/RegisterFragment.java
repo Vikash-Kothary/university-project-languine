ca81b84c30d0a1e7ff9f0e042cbed637c5a33420
Vikash Kothary <kothary.vikash@gmail.com>
2016-03-28 23:16:33 +0100

    fixed register button crash

--- app/src/main/java/com/puzzle/languine/ui/fragment/RegisterFragment.java
+++ app/src/main/java/com/puzzle/languine/ui/fragment/RegisterFragment.java
@@ -2,11 +2,13 @@ package com.puzzle.languine.ui.fragment;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.support.v7.widget.AppCompatImageButton;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 import com.parse.ParseException;
@@ -31,7 +33,7 @@ public class RegisterFragment extends MaterialFragment implements View.OnClickLi
 
         getToolbar().setTitle("Register");
 
-        Button button_register = (Button) findViewById(R.id.button_register);
+        ImageButton button_register = (ImageButton) findViewById(R.id.button_register);
         button_register.setOnClickListener(this);
 
         // TODO: check password and confirm_password are the same
